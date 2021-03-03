67e49e67ff1272a92b5723d0a3bec0874636d875
flux-macOS <jpascasio1993@gmail.com>
2021-03-03 16:46:47 +0800

    [UPDATE] minor updates

--- app/src/test/java/com/exam/weather_forecast/WeatherRepositoryTest.kt
+++ app/src/test/java/com/exam/weather_forecast/WeatherRepositoryTest.kt
@@ -1,11 +1,6 @@
 package com.exam.weather_forecast
 
-import android.content.Context
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import com.exam.weather_forecast.data.database.AppDatabase
 import com.exam.weather_forecast.data.database.dao.WeatherDAO
-import com.exam.weather_forecast.data.domain.LocalWeatherDTO
 import com.exam.weather_forecast.data.domain.RemoteGroupWeatherDTO
 import com.exam.weather_forecast.data.domain.Result
 import com.exam.weather_forecast.data.domain.Weather
@@ -15,23 +10,22 @@ import com.exam.weather_forecast.data.services.IWeatherService
 import io.mockk.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
+import java.io.IOException
 
-@RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class WeatherRepositoryTest {
-    private lateinit var appDatabase: AppDatabase
+
     private lateinit var dao: WeatherDAO
     private lateinit var weatherService: IWeatherService
     private lateinit var sampleWeathers: List<Weather>
     private lateinit var repository: IWeatherRepository
+
     @Before
     fun setUpMocks() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        appDatabase = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
-        dao = appDatabase.weatherDAO()
+        dao = mockk(relaxed = true)
         weatherService = mockk()
         sampleWeathers = listOf(
             Weather(
@@ -63,32 +57,63 @@ class WeatherRepositoryTest {
             )
         )
         repository = WeatherRepositoryImpl(dao, weatherService)
-
     }
 
     @Test
-    fun `getweather from remote`() = runBlockingTest(Dispatchers.Unconfined)  {
+    fun `get weather from remote`() = runBlockingTest  {
 
         val weather = sampleWeathers[0]
-        val localWeatherDTO = LocalWeatherDTO(
-            weather.id,
-            weather.city,
-            weather.status,
-            weather.tempMin,
-            weather.tempMax,
-            weather.temp,
-            weather.favorite
-        )
         val remoteDTO: RemoteGroupWeatherDTO.WeatherDTO = mockk(relaxed = true)
         val cityId = weather.id
 
         coEvery { weatherService.getWeather(cityId) } returns remoteDTO
-        coEvery { dao.isFavorite(cityId) } returns true
+
         val result = repository.getWeatherRemote(cityId)
+
         coVerify {
              repository.getWeatherRemote(cityId)
-//            dao.insertWeathers(listOf(localWeatherDTO))
         }
-        assert(result is Result.Success<*>) { "should produce Success type Result $result" }
+
+        assert(result is Result.Success<*>) { "Should return Result<Weather>" }
     }
+
+    @Test
+    fun `retrieve weathers from remote and update local`() = runBlockingTest {
+        val remoteDTO: RemoteGroupWeatherDTO = mockk(relaxed = true)
+        val cityIds = sampleWeathers.map { it.id }
+        val joinedIds = cityIds.joinToString(",")
+        coEvery { weatherService.getWeathers(joinedIds) } returns remoteDTO
+
+        val result = repository.updateLocalWeathers(cityIds)
+
+        coVerify {
+            repository.updateLocalWeathers(cityIds)
+        }
+
+        assert(result is Result.Success<*>) { "Should return Result<Unit>" }
+    }
+
+    @Test
+    fun `should throw exception upon remote service disruption`() = runBlockingTest {
+        val weather = sampleWeathers[0]
+        val cityId = weather.id
+
+        val exception = IOException("Unable to resolve host")
+
+        coEvery { weatherService.getWeather(cityId) } throws exception
+
+        val result = repository.getWeatherRemote(cityId)
+
+        coVerify {
+            repository.getWeatherRemote(cityId)
+        }
+
+        assert(result is Result.Error) { "returns Result.Error with an exception" }
+    }
+
+    @After
+    fun clearMock() {
+        clearAllMocks()
+    }
+
 }
